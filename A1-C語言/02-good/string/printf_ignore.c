ba788b538295b7a4615101dca4b0a09587fdd815
ccckmit <ccckmit@gmail.com>
2023-02-22 11:51:32 +0800

    before 111b week2

--- A1-C語言/02-good/string/printf_ignore.c
+++ A1-C語言/02-good/string/printf_ignore.c
@@ -2,6 +2,7 @@
 // printf 的 %*s 和我們想的不一樣！
 // 參考 -- https://stackoverflow.com/questions/39949473/forcing-printf-to-ignore-formatting
 int main() {
-  char *a="hello", *b="world";
-  printf("a=%*s b=%s\n", a, b, b);
+  char *a="hello xxxx", *b="world";
+  // printf("a=%*s b=%s\n", a, b, b);
+  printf("a=%.*s b=%s\n", 3, a, b);
 }
---
+++ A1-C語言/02-good/string/unicode_win.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
+int main() {
+    system("chcp 65001");
+    printf("Hello 你好！\n");
+}
