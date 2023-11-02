887be4f331e791e905653e0907cb4fc831017092
ccckmit <ccckmit@gmail.com>
2023-11-02 11:40:53 +0800

    comment multGoto.c

--- 03-nand2tetris/04/mult/multGoto.c
+++ 03-nand2tetris/04/mult/multGoto.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int R0 = 3;
-    int R1 = 5;
+    int R0 = 3; // 測試程式會設，自己不要去設定
+    int R1 = 5; // 測試程式會設，自己不要去設定
     int R2 = 0; // @2 M=0,
     
 //    while (R0 > 0) {
