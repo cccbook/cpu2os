c94666e50e6e93eca49802e3dcac5299267f6d0b
ccckmit <ccckmit@gmail.com>
2023-12-21 10:37:04 +0800

    fix hola.s

--- 05-x86/01-asm/02-hola/hola.s
+++ 05-x86/01-asm/02-hola/hola.s
@@ -10,12 +10,7 @@
         .text
 main:
         push    %rbx                    # we have to save this since we use it
-
-        mov     $90, %ecx               # ecx will countdown to 0
-        xor     %rax, %rax              # rax will hold the current number
-        xor     %rbx, %rbx              # rbx will hold the next number
-        inc     %rbx                    # rbx is originally 1
-        
+   
         mov     $message, %rdi          # First integer (or pointer) parameter in %rdi
         call    puts                    # puts(message)
 
