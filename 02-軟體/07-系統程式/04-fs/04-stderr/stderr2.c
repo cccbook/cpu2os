155d8fad9b9e8f224ed90828759f64951f5d7d52
ccckmit <ccckmit@gmail.com>
2024-04-24 16:02:58 +0800

    modify stderr2.c

--- 02-軟體/07-系統程式/04-fs/04-stderr/stderr2.c
+++ 02-軟體/07-系統程式/04-fs/04-stderr/stderr2.c
@@ -5,6 +5,7 @@
 int main() {
   int fdb = open("log.txt", O_APPEND|O_CREAT|O_RDWR, 0644);  // 打開檔案 log.txt 並取得代號 fdb
   dup2(fdb, 2); // 複製 fdb 檔案描述子到 2 (stderr)
+  fprintf(stdout, "Hello World!\n");
   fprintf(stderr, "Warning: xxx\n");
   fprintf(stderr, "Error: yyy\n");
 }
