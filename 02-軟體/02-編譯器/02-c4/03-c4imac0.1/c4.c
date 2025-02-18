042b484c8d3567c0ebcf0e041f60893cff998fbd
ccckmit <ccckmit@gmail.com>
2025-02-18 09:04:34 +0800

    c4 mac 0.1

--- 02-軟體/02-編譯器/02-c4/03-c4imac0.1/c4.c
+++ 02-軟體/02-編譯器/02-c4/03-c4imac0.1/c4.c
@@ -7,18 +7,12 @@
 // Written by Robert Swierczek
 // 修改者: 陳鍾誠 (模組化並加上中文註解)
  
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
 #include <unistd.h>
 #include <fcntl.h>
-
-long long compile(long long argc, char **argv);
-
-int main(int argc, char **argv) { // 主程式
-  compile(argc, argv);
-}
+#include <stdint.h>
 
 #define int long long // 64 bit 電腦
 
@@ -152,14 +146,6 @@ void expr(int lev) // 運算式 expression, 其中 lev 代表優先等級
     while (tk == '"') next();
     data = (char *)((int)data + sizeof(int) & -sizeof(int)); ty = PTR; // 用 int 為大小對齊 ??
   }
-  else if (tk == Sizeof) { // 處理 sizeof(type) ，其中 type 可能為 char, int 或 ptr
-    next(); if (tk == '(') next(); else { printf("%d: open paren expected in sizeof\n", line); exit(-1); }
-    ty = INT; if (tk == Int) next(); else if (tk == Char) { next(); ty = CHAR; }
-    while (tk == Mul) { next(); ty = ty + PTR; }
-    if (tk == ')') next(); else { printf("%d: close paren expected in sizeof\n", line); exit(-1); }
-    *++e = IMM; *++e = (ty == CHAR) ? sizeof(char) : sizeof(int);
-    ty = INT;
-  }
   else if (tk == Id) { // 處理 id ...
     d = id; next();
     if (tk == '(') { // id (args) ，這是 call
@@ -182,152 +168,11 @@ void expr(int lev) // 運算式 expression, 其中 lev 代表優先等級
       *++e = ((ty = d[Type]) == CHAR) ? LC : LI; // LI  : load int, LC  : load char
     }
   }
-  else if (tk == '(') { // (E) : 有括號的運算式 ...
-    next();
-    if (tk == Int || tk == Char) {
-      t = (tk == Int) ? INT : CHAR; next();
-      while (tk == Mul) { next(); t = t + PTR; }
-      if (tk == ')') next(); else { printf("%d: bad cast\n", line); exit(-1); }
-      expr(Inc); // 處理 ++, -- 的情況
-      ty = t;
-    }
-    else {
-      expr(Assign); // 處理 (E) 中的 E      (E 運算式必須能處理 (t=x) op y 的情況，所以用 expr(Assign))
-      if (tk == ')') next(); else { printf("%d: close paren expected\n", line); exit(-1); }
-    }
-  }
-  else if (tk == Mul) { // * 乘法
-    next(); expr(Inc);
-    if (ty > INT) ty = ty - PTR; else { printf("%d: bad dereference\n", line); exit(-1); }
-    *++e = (ty == CHAR) ? LC : LI;
-  }
-  else if (tk == And) { // & AND
-    next(); expr(Inc);
-    if (*e == LC || *e == LI) --e; else { printf("%d: bad address-of\n", line); exit(-1); }
-    ty = ty + PTR;
-  }
-  else if (tk == '!') { next(); expr(Inc); *++e = PSH; *++e = IMM; *++e = 0; *++e = EQ; ty = INT; } // NOT
-  else if (tk == '~') { next(); expr(Inc); *++e = PSH; *++e = IMM; *++e = -1; *++e = XOR; ty = INT; } // Logical NOT
-  else if (tk == Add) { next(); expr(Inc); ty = INT; }
-  else if (tk == Sub) {
-    next(); *++e = IMM;
-    if (tk == Num) { *++e = -ival; next(); } else { *++e = -1; *++e = PSH; expr(Inc); *++e = MUL; } // -Num or -E
-    ty = INT;
-  }
-  else if (tk == Inc || tk == Dec) { // ++ or --
-    t = tk; next(); expr(Inc);
-    if (*e == LC) { *e = PSH; *++e = LC; }
-    else if (*e == LI) { *e = PSH; *++e = LI; }
-    else { printf("%d: bad lvalue in pre-increment\n", line); exit(-1); }
-    *++e = PSH;
-    *++e = IMM; *++e = (ty > PTR) ? sizeof(int) : sizeof(char);
-    *++e = (t == Inc) ? ADD : SUB;
-    *++e = (ty == CHAR) ? SC : SI;
-  }
-  else { printf("%d: bad expression\n", line); exit(-1); }
-  // 參考: https://en.wikipedia.org/wiki/Operator-precedence_parser, https://www.cnblogs.com/rubylouvre/archive/2012/09/08/2657682.html https://web.archive.org/web/20151223215421/http://hall.org.ua/halls/wizzard/pdf/Vaughan.Pratt.TDOP.pdf
-  while (tk >= lev) { // "precedence climbing" or "Top Down Operator Precedence" method
-    t = ty;
-    if (tk == Assign) {
-      next();
-      if (*e == LC || *e == LI) *e = PSH; else { printf("%d: bad lvalue in assignment\n", line); exit(-1); }
-      expr(Assign); *++e = ((ty = t) == CHAR) ? SC : SI;
-    }
-    else if (tk == Cond) {
-      next();
-      *++e = BZ; d = ++e;
-      expr(Assign);
-      if (tk == ':') next(); else { printf("%d: conditional missing colon\n", line); exit(-1); }
-      *d = (int)(e + 3); *++e = JMP; d = ++e;
-      expr(Cond);
-      *d = (int)(e + 1);
-    }
-    else if (tk == Lor) { next(); *++e = BNZ; d = ++e; expr(Lan); *d = (int)(e + 1); ty = INT; }
-    else if (tk == Lan) { next(); *++e = BZ;  d = ++e; expr(Or);  *d = (int)(e + 1); ty = INT; }
-    else if (tk == Or)  { next(); *++e = PSH; expr(Xor); *++e = OR;  ty = INT; }
-    else if (tk == Xor) { next(); *++e = PSH; expr(And); *++e = XOR; ty = INT; }
-    else if (tk == And) { next(); *++e = PSH; expr(Eq);  *++e = AND; ty = INT; }
-    else if (tk == Eq)  { next(); *++e = PSH; expr(Lt);  *++e = EQ;  ty = INT; }
-    else if (tk == Ne)  { next(); *++e = PSH; expr(Lt);  *++e = NE;  ty = INT; }
-    else if (tk == Lt)  { next(); *++e = PSH; expr(Shl); *++e = LT;  ty = INT; }
-    else if (tk == Gt)  { next(); *++e = PSH; expr(Shl); *++e = GT;  ty = INT; }
-    else if (tk == Le)  { next(); *++e = PSH; expr(Shl); *++e = LE;  ty = INT; }
-    else if (tk == Ge)  { next(); *++e = PSH; expr(Shl); *++e = GE;  ty = INT; }
-    else if (tk == Shl) { next(); *++e = PSH; expr(Add); *++e = SHL; ty = INT; }
-    else if (tk == Shr) { next(); *++e = PSH; expr(Add); *++e = SHR; ty = INT; }
-    else if (tk == Add) {
-      next(); *++e = PSH; expr(Mul);
-      if ((ty = t) > PTR) { *++e = PSH; *++e = IMM; *++e = sizeof(int); *++e = MUL;  }
-      *++e = ADD;
-    }
-    else if (tk == Sub) {
-      next(); *++e = PSH; expr(Mul);
-      if (t > PTR && t == ty) { *++e = SUB; *++e = PSH; *++e = IMM; *++e = sizeof(int); *++e = DIV; ty = INT; }
-      else if ((ty = t) > PTR) { *++e = PSH; *++e = IMM; *++e = sizeof(int); *++e = MUL; *++e = SUB; }
-      else *++e = SUB;
-    }
-    else if (tk == Mul) { next(); *++e = PSH; expr(Inc); *++e = MUL; ty = INT; }
-    else if (tk == Div) { next(); *++e = PSH; expr(Inc); *++e = DIV; ty = INT; }
-    else if (tk == Mod) { next(); *++e = PSH; expr(Inc); *++e = MOD; ty = INT; }
-    else if (tk == Inc || tk == Dec) {
-      if (*e == LC) { *e = PSH; *++e = LC; }
-      else if (*e == LI) { *e = PSH; *++e = LI; }
-      else { printf("%d: bad lvalue in post-increment\n", line); exit(-1); }
-      *++e = PSH; *++e = IMM; *++e = (ty > PTR) ? sizeof(int) : sizeof(char);
-      *++e = (tk == Inc) ? ADD : SUB;
-      *++e = (ty == CHAR) ? SC : SI;
-      *++e = PSH; *++e = IMM; *++e = (ty > PTR) ? sizeof(int) : sizeof(char);
-      *++e = (tk == Inc) ? SUB : ADD;
-      next();
-    }
-    else if (tk == Brak) {
-      next(); *++e = PSH; expr(Assign);
-      if (tk == ']') next(); else { printf("%d: close bracket expected\n", line); exit(-1); }
-      if (t > PTR) { *++e = PSH; *++e = IMM; *++e = sizeof(int); *++e = MUL;  }
-      else if (t < PTR) { printf("%d: pointer type expected\n", line); exit(-1); }
-      *++e = ADD;
-      *++e = ((ty = t - PTR) == CHAR) ? LC : LI;
-    }
-    else { printf("%d: compiler error tk=%d\n", line, tk); exit(-1); }
-  }
 }
 
 void stmt() // 陳述 statement
 {
-  int *a, *b;
-
-  if (tk == If) { // if 語句
-    next();
-    if (tk == '(') next(); else { printf("%d: open paren expected\n", line); exit(-1); }
-    expr(Assign);
-    if (tk == ')') next(); else { printf("%d: close paren expected\n", line); exit(-1); }
-    *++e = BZ; b = ++e;
-    stmt();
-    if (tk == Else) { // else 語句
-      *b = (int)(e + 3); *++e = JMP; b = ++e;
-      next();
-      stmt();
-    }
-    *b = (int)(e + 1);
-  }
-  else if (tk == While) { // while 語句
-    next();
-    a = e + 1;
-    if (tk == '(') next(); else { printf("%d: open paren expected\n", line); exit(-1); }
-    expr(Assign);
-    if (tk == ')') next(); else { printf("%d: close paren expected\n", line); exit(-1); }
-    *++e = BZ; b = ++e;
-    stmt();
-    *++e = JMP; *++e = (int)a;
-    *b = (int)(e + 1);
-  }
-  else if (tk == Return) { // return 語句
-    next();
-    if (tk != ';') expr(Assign);
-    *++e = LEV;
-    if (tk == ';') next(); else { printf("%d: semicolon expected\n", line); exit(-1); }
-  }
-  else if (tk == '{') { // 區塊 {...}
+  if (tk == '{') { // 區塊 {...}
     next();
     while (tk != '}') stmt();
     next();
@@ -349,27 +194,7 @@ int prog() { // 編譯整個程式 Program
     bt = INT; // basetype
     if (tk == Int) next();
     else if (tk == Char) { next(); bt = CHAR; }
-    else if (tk == Enum) { // enum Id? {... 列舉
-      next();
-      if (tk != '{') next(); // 略過 Id
-      if (tk == '{') {
-        next();
-        i = 0; // 紀錄 enum 的目前值
-        while (tk != '}') {
-          if (tk != Id) { printf("%d: bad enum identifier %d\n", line, tk); return -1; }
-          next();
-          if (tk == Assign) { // 有 Id=Num 的情況
-            next();
-            if (tk != Num) { printf("%d: bad enum initializer\n", line); return -1; }
-            i = ival;
-            next();
-          }
-          id[Class] = Num; id[Type] = INT; id[Val] = i++;
-          if (tk == ',') next();
-        }
-        next();
-      }
-    }
+    else if (tk == Enum) { printf("%d: enum unsupport!\n", line); exit(-1); } // ccc
     while (tk != ';' && tk != '}') { // 掃描直到區塊結束
       ty = bt;
       while (tk == Mul) { next(); ty = ty + PTR; }
@@ -382,40 +207,11 @@ int prog() { // 編譯整個程式 Program
         id[Val] = (int)(e + 1);
         next(); i = 0;
         while (tk != ')') { // 掃描參數直到 ...)
-          ty = INT;
-          if (tk == Int) next();
-          else if (tk == Char) { next(); ty = CHAR; }
-          while (tk == Mul) { next(); ty = ty + PTR; }
-          if (tk != Id) { printf("%d: bad parameter declaration\n", line); return -1; }
-          if (id[Class] == Loc) { printf("%d: duplicate parameter definition\n", line); return -1; } // 這裡的 id 會指向 hash 搜尋過的 symTable 裏的那個 (在 next 裏處理的)，所以若是該 id 已經是 Local，那麼就重複了！
-          // 把 id.Class, id.Type, id.Val 暫存到 id.HClass, id.HType, id.Hval ，因為 Local 優先於 Global
-          id[HClass] = id[Class]; id[Class] = Loc;
-          id[HType]  = id[Type];  id[Type] = ty;
-          id[HVal]   = id[Val];   id[Val] = i++;
-          next();
-          if (tk == ',') next();
         }
         next();
         if (tk != '{') { printf("%d: bad function definition\n", line); return -1; } // BODY 開始 {...
         loc = ++i;
         next();
-        while (tk == Int || tk == Char) { // 宣告
-          bt = (tk == Int) ? INT : CHAR;
-          next();
-          while (tk != ';') {
-            ty = bt;
-            while (tk == Mul) { next(); ty = ty + PTR; }
-            if (tk != Id) { printf("%d: bad local declaration\n", line); return -1; }
-            if (id[Class] == Loc) { printf("%d: duplicate local definition\n", line); return -1; }
-            // 把 id.Class, id.Type, id.Val 暫存到 id.HClass, id.HType, id.Hval ，因為 Local 優先於 Global
-            id[HClass] = id[Class]; id[Class] = Loc;
-            id[HType]  = id[Type];  id[Type] = ty;
-            id[HVal]   = id[Val];   id[Val] = ++i;
-            next();
-            if (tk == ',') next();
-          }
-          next();
-        }
         *++e = ENT; *++e = i - loc;
         while (tk != '}') stmt();
         *++e = LEV;
@@ -550,3 +346,6 @@ int compile(int argc, char **argv) // 主程式
   return run(pc, bp, sp);
 }
 
+int main(int32_t argc, char **argv) { // 主程式
+  compile(argc, argv);
+}
--- 02-軟體/02-編譯器/02-c4/09b-c4imac/test.sh
+++ 02-軟體/02-編譯器/02-c4/09b-c4imac/test.sh
@@ -3,5 +3,5 @@ gcc -w c4.c -o c4
 ./c4 -s test/fib.c
 ./c4 test/fib.c
 ./c4 hello.c
-# ./c4 c4.c hello.c
-# ./c4 c4.c c4.c hello.c
+./c4 c4.c hello.c
+./c4 c4.c c4.c hello.c
